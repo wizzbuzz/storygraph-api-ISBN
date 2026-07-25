218839f366198462635e764b4df883ee10349601
WizzBuzz <joop_hooijmans@outlook.com>
2026-07-25 23:16:25 +0200

    Added options for author, format, language, and pub date.

--- storygraph_api/parse/user_parser.py
+++ storygraph_api/parse/user_parser.py
@@ -6,20 +6,33 @@ import re
 class UserParser:
     @staticmethod
     @parsing_exception 
-    def parse_html(html,get_id, get_isbn):
+    def parse_html(html,get_id, get_isbn, get_format, get_language, get_pub_date, get_author):
         soup = BeautifulSoup(html, 'html.parser')
         books_list = []
         books = list({element.parent for element in soup.find_all(class_="book-title-author-and-series") if element.parent})
         for book in books:
             book_details = {}
-            title = book.find('a').text.strip()
+            title = book.find('a').text
             book_details["title"] = title
+            if(get_author):
+                book_author = book.select_one('a[href*="author"]').text
+                book_details["book_author"] = book_author
+
             if(get_id):
                 book_id = book.find('a')['href'].split('/')[-1]
                 book_details["book_id"] = book_id
             if(get_isbn):
-                book_isbn = soup.find('span', string=re.compile(r'isbn', re.IGNORECASE)).parent.text.split(" ")[1]
+                book_isbn = book.find('span', string=re.compile(r'isbn', re.IGNORECASE)).parent.text.split(":")[1][1::]
                 book_details["book_isbn"] = book_isbn
+            if(get_format):
+                book_format = book.find('span', string=re.compile(r'format', re.IGNORECASE)).parent.text.split(":")[1][1::]
+                book_details["book_format"] = book_format
+            if(get_language):
+                book_language = book.find('span', string=re.compile(r'language', re.IGNORECASE)).parent.text.split(":")[1][1::]
+                book_details["book_language"] = book_language
+            if(get_pub_date):
+                book_pub_date = book.find('span', string=re.compile(r'edition pub date', re.IGNORECASE)).parent.text.split(":")[1][1::]
+                book_details["book_pub_date"] = book_pub_date
 
             books_list.append(book_details)
         data = list({
@@ -29,16 +42,16 @@ class UserParser:
         return data
 
     @staticmethod
-    def currently_reading(uname, cookie,get_id, get_isbn):
+    def currently_reading(uname, cookie,get_id, get_isbn, get_format, get_language, get_pub_date, get_author):
         content = UserScraper.currently_reading(uname,cookie)
-        return UserParser.parse_html(content,get_id, get_isbn)
+        return UserParser.parse_html(content,get_id, get_isbn, get_format, get_language, get_pub_date, get_author)
 
     @staticmethod
-    def to_read(uname, cookie,get_id, get_isbn):
+    def to_read(uname, cookie,get_id, get_isbn, get_format, get_language, get_pub_date, get_author):
         content = UserScraper.to_read(uname,cookie)
-        return UserParser.parse_html(content,get_id, get_isbn)
+        return UserParser.parse_html(content,get_id, get_isbn, get_format, get_language, get_pub_date, get_author)
 
     @staticmethod
-    def books_read(uname, cookie,get_id, get_isbn):
+    def books_read(uname, cookie,get_id, get_isbn, get_format, get_language, get_pub_date, get_author):
         content = UserScraper.books_read(uname,cookie)
-        return UserParser.parse_html(content,get_id, get_isbn)
+        return UserParser.parse_html(content,get_id, get_isbn, get_format, get_language, get_pub_date, get_author)
--- storygraph_api/users_client.py
+++ storygraph_api/users_client.py
@@ -4,16 +4,16 @@ import json
 
 class User:
     @handle_exceptions
-    def currently_reading(self,uname,cookie,get_id = False, get_isbn = False):
-        data = UserParser.currently_reading(uname,cookie, get_id, get_isbn)
+    def currently_reading(self,uname,cookie,get_id = False, get_isbn = False, get_format = False, get_language = False, get_pub_date = False, get_author = False):
+        data = UserParser.currently_reading(uname,cookie, get_id, get_isbn, get_format, get_language, get_pub_date, get_author)
         return json.dumps(data,indent=4)
 
     @handle_exceptions
-    def to_read(self,uname,cookie,get_id = False, get_isbn = False):
-        data = UserParser.to_read(uname,cookie,get_id, get_isbn)
+    def to_read(self,uname,cookie,get_id = False, get_isbn = False, get_format = False, get_language = False, get_pub_date = False, get_author = False):
+        data = UserParser.to_read(uname,cookie,get_id, get_isbn, get_format, get_language, get_pub_date, get_author)
         return json.dumps(data,indent=4)
 
     @handle_exceptions
-    def books_read(self,uname,cookie,get_id = False, get_isbn = False):
-        data = UserParser.books_read(uname,cookie,get_id, get_isbn)
+    def books_read(self,uname,cookie,get_id = False, get_isbn = False, get_format = False, get_language = False, get_pub_date = False, get_author = False):
+        data = UserParser.books_read(uname,cookie,get_id, get_isbn, get_format, get_language, get_pub_date, get_author)
         return json.dumps(data,indent=4)
