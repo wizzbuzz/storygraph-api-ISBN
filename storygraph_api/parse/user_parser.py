37adc0b3a92f5f32135afd9f5f3b5fd2243061cc
WizzBuzz <joop_hooijmans@outlook.com>
2026-07-25 23:46:12 +0200

    Improved book_id query, cleaned code

--- storygraph_api/parse/user_parser.py
+++ storygraph_api/parse/user_parser.py
@@ -16,27 +16,29 @@ class UserParser:
             title = book.find('a').text
             book_details["title"] = title
             if(get_author):
-                book_author = book.select_one('a[href*="author"]').text
-                book_details["book_author"] = book_author
+                try:
+                    book_author = book.select_one('a[href*="author"]').text
+                    book_details["book_author"] = book_author
+                except:
+                    book_details["book_author"] = None
 
             if(get_id):
-                book_id = book.find('a')['href'].split('/')[-1]
-                book_details["book_id"] = book_id
-            if(get_isbn):
-                book_isbn = book.find('span', string=re.compile(r'isbn', re.IGNORECASE)).parent.text.split(":")[1][1::]
-                book_details["book_isbn"] = book_isbn
-            if(get_format):
-                book_format = book.find('span', string=re.compile(r'format', re.IGNORECASE)).parent.text.split(":")[1][1::]
-                book_details["book_format"] = book_format
-            if(get_language):
-                book_language = book.find('span', string=re.compile(r'language', re.IGNORECASE)).parent.text.split(":")[1][1::]
-                book_details["book_language"] = book_language
+                try:
+                    book_id = book.find('div', class_='edition-info').get('data-book-id')
+                    book_details["book_id"] = book_id
+                except:
+                    book_details["book_id"] = None
+
             if(get_pub_date):
                 try:
-                    book_pub_date = book.find('span', string=re.compile(r'edition pub date', re.IGNORECASE)).parent.text.split(":")[1][1::]
+                    book_pub_date = UserParser.get_detail(book, "edition pub date")
                     book_details["book_pub_date"] = datetime.strptime(book_pub_date, "%d %b %Y").isoformat().split("T")[0]
                 except:
                     book_details["book_pub_date"] = None
+
+            UserParser.add_detail_to_list(get_isbn, book, book_details, "book_isbn", "isbn")
+            UserParser.add_detail_to_list(get_format, book, book_details, "book_format", "format")
+            UserParser.add_detail_to_list(get_format, book, book_details, "book_language", "language")
 
             books_list.append(book_details)
         data = list({
@@ -44,6 +46,19 @@ class UserParser:
             for book in books_list
             }.values())
         return data
+
+    @staticmethod
+    def add_detail_to_list(detail_boolean, book, book_details, book_detail_name, query):
+        if(detail_boolean):
+            try:
+                book_isbn = UserParser.get_detail(book, query)
+                book_details[book_detail_name] = book_isbn
+            except:
+                book_details[book_detail_name] = None
+
+    @staticmethod
+    def get_detail(book, name):
+        return book.find('span', string=re.compile(rf'{name}', re.IGNORECASE)).parent.text.split(":")[1][1::]
 
     @staticmethod
     def currently_reading(uname, cookie,get_id, get_isbn, get_format, get_language, get_pub_date, get_author):
